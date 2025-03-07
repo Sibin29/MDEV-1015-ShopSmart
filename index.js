d3e4c1d2639c3efda2cb8623296a6c0b37224e82
JoeAsh7 <ashikjoesavio@outlook.com>
2025-03-07 05:58:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './App';  // Import App component
 import { name as appName } from './app.json';  // Import appName from app.json
-
 AppRegistry.registerComponent(appName, () => App);  // Register the App component
