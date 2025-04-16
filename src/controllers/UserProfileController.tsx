b8dcf5d969ea934a0af6ebbb0c4046c0a441c6db
JoeAsh7 <ashikjoesavio@outlook.com>
2025-04-16 07:58:25 -0300

    Updated user profile screen

--- src/controllers/UserProfileController.tsx
+++ src/controllers/UserProfileController.tsx
@@ -1,9 +1,25 @@
+import { getAuth } from 'firebase/auth';
+
 export const getUserProfile = () => {
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  if (user) {
     return {
-      username: 'Shop Smart',
-      email: 'ShopSmart@example.com',
+      // create username by splitting the email before the '@' sign and taking the first part
+      username: user.email ? user.email.split('@')[0] : 'ShopSmart User',
+      email: user.email || 'No Email',
       address: '123 Main St, Barrie',
+      profileImage:
+        user.photoURL ||
+        'https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg',
+    };
+  } else {
+    return {
+      username: 'Guest',
+      email: 'Not logged in',
+      address: '',
       profileImage: 'https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg',
     };
-  };
-  
+  }
+};
